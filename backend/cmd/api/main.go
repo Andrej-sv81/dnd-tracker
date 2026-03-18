f8195a0f91c8902fa94b7e5fa111e63193ae9000
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2026-03-18 22:10:49 +0100

    Initial project setup: Go back + postgres

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	log.Printf("Starting server on port %s", port)
+	router.Run(":" + port)
+
+}
